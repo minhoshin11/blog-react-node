84d1216e3d48e9534cd2a514317e105dc5c41de3
minhoshin11 <tlsalsgh777@naver.com>
2024-10-11 13:50:22 +0900

    로그인 모달 구현

---
+++ front-end/src/components/Home/LoginModal.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef } from "react";
+import { IoCloseSharp } from "react-icons/io5";
+
+interface LoginModalProps {
+  toggleLogin: boolean;
+  setToggleLogin: (value: boolean) => void;
+}
+
+export default function LoginModal({
+  toggleLogin,
+  setToggleLogin,
+}: LoginModalProps) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+      setToggleLogin(false);
+    }
+  };
+
+  useEffect(() => {
+    if (toggleLogin) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [toggleLogin]);
+
+  return (
+    <div
+      ref={modalRef}
+      className={`${
+        toggleLogin ? "block" : "hidden"
+      } flex flex-col items-center gap-8 absolute border-2 border-gray-600 mt-[20vw] w-[400px] h-[590px] rounded-md`}
+    >
+      <div className="w-full flex justify-end">
+        <IoCloseSharp
+          onClick={() => setToggleLogin(false)}
+          className="mt-2 mr-2 mb-2 w-[40px] h-auto text-gray-500"
+        />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <h2 className="text-4xl font-bold">Tistory</h2>
+        <p className="text-gray-400">당신의 이야기가 값진 수익이 됩니다.</p>
+      </div>
+      <img src="/vite.svg" alt="이미지사진예시" className="w-[150px] h-auto" />
+      <div className="border">일반로그인</div>
+      <div className="border bg-yellow-300">카카오 계정 로그인</div>
+      <div className="flex flex-col items-center gap-2">
+        <div className="text-gray-400">내 스토리 계정을 모르겠어요</div>
+        <div className="text-gray-400">
+          티스토리 이메일 계정으로 가입했었나요?
+        </div>
+      </div>
+    </div>
+  );
+}
--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+import LeftSection from "../components/Home/LeftSection";
+import LoginModal from "../components/Home/LoginModal";
+import RightSection from "../components/Home/RightSection";
+
 export default function Home() {
+  const [toggleLogin, setToggleLogin] = useState(true);
+  const handleLoginModal = () => {
+    setToggleLogin(!toggleLogin);
+    console.log(toggleLogin);
+  };
+
   return (
-    <section className="flex justify-center">
+    <section className="flex justify-center relative">
       <div className="w-auto flex gap-1">
-        <section className="w-1/2 border-r border-black">왼쪽</section>
-        <section className="w-1/2 whitespace-nowrap">오른쪽</section>
+        <section className="w-3/5 border-r border-black px-8 py-2">
+          <LeftSection />
+        </section>
+        <section className="w-2/5  px-8 py-2">
+          <RightSection />
+        </section>
+        <div className="hover:cursor-pointer" onClick={handleLoginModal}>
+          대충 로그인모달
+        </div>
       </div>
+      <LoginModal toggleLogin={toggleLogin} setToggleLogin={setToggleLogin} />
     </section>
   );
 }
